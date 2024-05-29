1379a19a7a5fc631794d6585acbc11cf78bcfb23
Natalia Rodrigues Costa <nanaz2007costa@gmail.com>
2024-05-29 00:54:54 +0000

    Alteração dinamica do assunto da página

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -8,3 +8,20 @@ botaoTema.addEventListener("click", () => {
 })
 
 verificarTema(body, botaoTema)
+
+const assunto= localStorage.getItem("assunto")
+
+console.log(assunto.toLowerCase())
+
+function alterarAssunto(){
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLocaleLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLocaleLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `icone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+}
+
+alterarAssunto()
